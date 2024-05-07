dea203de80c1abab0a489d112380097a92a94358
Aleksandr Sundeev <navigator97@mail.ru>
2024-05-07 18:45:38 +0700

    Add models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': 'True', 'null': 'True'}
+
+
+class Product(models.Model):
+    pass
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['category', 'name']
+
+
+class Category(models.Model):
+    pass
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+        ordering = ["name"]
