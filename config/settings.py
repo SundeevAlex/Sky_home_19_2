332863f975e58ccf95a9f862cade47d86374066d
Aleksandr Sundeev <navigator97@mail.ru>
2024-05-09 05:41:57 +0700

    Add MEDIA & static

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -110,7 +111,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIR = (BASE_DIR / 'static',)
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
