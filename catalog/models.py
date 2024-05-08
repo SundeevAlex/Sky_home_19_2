0275ece10d7b4e95c21b035ac24a8e1d43a8c98a
Aleksandr Sundeev <navigator97@mail.ru>
2024-05-08 17:11:31 +0700

    makemigrations N1 with model Product&Category

--- catalog/models.py
+++ catalog/models.py
@@ -4,7 +4,23 @@ NULLABLE = {'blank': 'True', 'null': 'True'}
 
 
 class Product(models.Model):
-    pass
+    name = models.CharField(max_length=50, verbose_name="Наименование", help_text='Введите наименование продукта')
+    description = models.TextField(verbose_name="Описание", help_text='Введите описание продукта', **NULLABLE)
+    image = models.ImageField(upload_to="media/photo", help_text='Загрузите фото продукта', **NULLABLE)
+    category = models.ForeignKey(
+        "Category",
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        **NULLABLE,
+        related_name="products",
+    )
+    price = models.IntegerField(verbose_name="Цена", help_text='Введите цену продукта', **NULLABLE)
+    created_at = models.DateField(
+        auto_created=True, verbose_name="Дата создания", **NULLABLE
+    )
+    updated_at = models.DateField(
+        auto_now_add=True, verbose_name="Дата изменения", **NULLABLE
+    )
 
     def __str__(self):
         return f'{self.name} {self.description}'
@@ -16,7 +32,8 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=50, verbose_name="Наименование", help_text='Введите наименование категории')
+    description = models.TextField(verbose_name="Описание", help_text='Введите описание категории', **NULLABLE)
 
     def __str__(self):
         return f'{self.name} {self.description}'
