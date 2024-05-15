69149da4c23df4c2e6c98ee86058647da2c2d538
Aleksandr Sundeev <navigator97@mail.ru>
2024-05-15 15:58:12 +0700

    Create products_list.html

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from catalog.apps import NewappConfig
-from catalog.views import index, home, contacts
+from catalog.views import products_list, home, contacts
 
 app_name = NewappConfig.name
 
 urlpatterns = [
-    path('', index),
+    path('', products_list),
     # path('', home, name='home'),
     path('contacts/', contacts, name='contacts')
 ]
--- config/settings.py
+++ config/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIR = (BASE_DIR / 'static',)
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
