c8915e5d20503e9ccf89310ea30b7552722c876e
Aleksandr Sundeev <navigator97@mail.ru>
2024-05-23 14:40:27 +0700

    Add to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from blog.models import Blog
 
@@ -9,19 +11,46 @@ class BlogCreateView(CreateView):
     fields = ('title', 'slug', 'content', 'preview', 'created_at')
     success_url = reverse_lazy('blog:list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_rec = form.save()
+            new_rec.slug = slugify(new_rec.title)
+            new_rec.save()
+        return super().form_valid(form)
+
 
 class BlogListView(ListView):
     model = Blog
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(published=True)
+        return queryset
 
 
 class BlogDetailView(DetailView):
     model = Blog
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views += 1
+        self.object.save()
+        return self.object
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'slug', 'content', 'preview', 'created_at')
-    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_rec = form.save()
+            new_rec.slug = slugify(new_rec.title)
+            new_rec.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
