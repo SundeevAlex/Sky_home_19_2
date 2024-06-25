9310119ccea5593a0c2aa4a5d001469ad2341ce9
Aleksandr Sundeev <navigator97@mail.ru>
2024-06-25 15:49:11 +0700

    Add StyleFormMixin

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,11 +1,22 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, BooleanField
 from catalog.models import Product, Version
 from django import forms
 
 prohibited_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    # pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, ModelForm):
 
     class Meta:
         model = Product
@@ -26,7 +37,7 @@ class ProductForm(ModelForm):
         return cleaned_description
 
 
-class VersionForm(ModelForm):
+class VersionForm(StyleFormMixin, ModelForm):
 
     class Meta:
         model = Version
