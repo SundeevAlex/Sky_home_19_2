c460b5d710d2bcc4034eb224c193d2115df0d0d3
Aleksandr Sundeev <navigator97@mail.ru>
2024-06-28 16:16:30 +0700

    Add verification

--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from users.forms import UserRegisterForm
+from django.urls import reverse, reverse_lazy
+from users.models import User
+import secrets
+from django.core.mail import send_mail
+from django.shortcuts import redirect, get_object_or_404
+from config.settings import EMAIL_HOST_USER
 
-# Create your views here.
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save()
+        user.is_active = False
+        token = secrets.token_hex(16)
+        user.token = token
+        user.save()
+        host = self.request.get_host()
+        url = f'http://{host}/users/email-confirm/{token}/'
+        send_mail(subject="Подтверждение почты",
+                  message=f"Перейдите по ссылке для подтверждения почты {url}",
+                  from_email='airnav@yandex.ru',
+                  recipient_list=[user.email]
+                  )
+        return super().form_valid(form)
+
+
+def email_verification(request, token):
+    user = get_object_or_404(User, token=token)
+    user.is_active = True
+    user.save()
+    return redirect(reverse('users:login'))
